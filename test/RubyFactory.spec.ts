a6b78a29582ddf8f89289fddeb034768c6c66647
VecDeque <tom.wilson87@abine.us>
2021-05-13 04:02:41 -0500

    Update gas usage and add package lock

--- test/RubyFactory.spec.ts
+++ test/RubyFactory.spec.ts
@@ -68,7 +68,7 @@ describe('RubyFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2509120)
+    expect(receipt.gasUsed).to.eq(2502886)
   })
 
   it('setFeeTo', async () => {
